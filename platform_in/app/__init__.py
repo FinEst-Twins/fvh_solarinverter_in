74ef162d2b64af26457bfcebdf0b728e649e90cf
Sheena Puthanpurayil <sheena24789@gmail.com>
2020-10-22 23:40:26 +0300

    kafka produce to different observations

--- platform_in/app/__init__.py
+++ platform_in/app/__init__.py
@@ -7,9 +7,15 @@ import json
 from confluent_kafka import avro
 from confluent_kafka.avro import AvroProducer
 import certifi
+#from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
+
+
 
 logging.basicConfig(level=logging.INFO)
 elastic_apm = ElasticAPM()
+
+#db = SQLAlchemy()
 
 success_response_object = {"status":"success"}
 success_code = 202
@@ -39,6 +45,37 @@ def kafka_avro_produce(avroProducer, topic, data):
 
     return True
 
+def get_ds_id(thing,sensor):
+
+    things = ["Inv1","Inv2","Inv3","Inv4","Inv5","Inv6","Inv7","Inv8"]
+    sensors = [ "VoltagePhase1","VoltagePhase2", "VoltagePhase3",
+
+     "VoltageString1",
+  "CurrentString1",
+  "OutputString1",
+  "VoltageString2",
+  "CurrentString2",
+  "OutputString2",
+  "VoltageString3",
+  "CurrentString3",
+  "OutputString3",
+
+  "CurrentPhase1",
+  "OutputPhase1",
+
+  "CurrentPhase2",
+  "OutputPhase2",
+
+"CurrentPhase3",
+"OutputPhase3",
+"TotalEnergy",
+"DailyEnery",
+"Status",
+"Fault"]
+
+    datastreams = list([(a, b) for a in things for b in sensors])
+    return datastreams.index((thing,sensor))+89
+
 def create_app(script_info=None):
 
     # instantiate the app
@@ -50,8 +87,9 @@ def create_app(script_info=None):
 
     # set up extensions
     elastic_apm.init_app(app)
+    #db.init_app(app)
 
-    value_schema = avro.load("avro/solarinverter.avsc")
+    value_schema = avro.load("avro/observation.avsc")
     avroProducer = AvroProducer(
         {
             "bootstrap.servers": app.config["KAFKA_BROKERS"],
@@ -80,16 +118,47 @@ def create_app(script_info=None):
     def post_solarinverter_data():
         try:
             data = request.get_json()
-            data = json.loads(data)
+
+            #data = json.loads(data)
             #print(data)
             logging.debug(f"post observation: {data}")
             #print("post data for solar inverter", data)
 
-            inverter_name = data["name"]
-            topic_prefix = "finest.viikkisolar"
+            topic_prefix = "finest-observations-viikkisolar"
 
-            topic = f"{topic_prefix}.{inverter_name}"
-            kafka_avro_produce(avroProducer, topic, data)
+            thing = data["name"]
+            timestamp = data["timestamp"]
+            dt_obj = datetime.strptime('2020-04-20T17:56:50.093538',
+                           '%Y-%m-%dT%H:%M:%S.%f')
+            timestamp_millisec = round(dt_obj.timestamp() * 1000)
+            del data["name"]
+            del data["timestamp"]
+            del data["type"]
+
+    #save type - name and time stampd
+    # for in keys
+    #query with inverter 1 and voltage string 1
+    #avro produce to obs with id
+            print(data)
+            for key,value in data.items():
+                sensor = key
+                print(sensor)
+                ds_id = get_ds_id(thing,sensor)
+                topic = f"{topic_prefix}"
+                observation = {
+                "phenomenontime_begin":None,
+                "phenomenontime_end":None,
+                "resulttime":timestamp_millisec,
+                "result":f"{value}",
+                "resultquality":None,
+                "validtime_begin":None,
+                "validtime_end":None,
+                "parameters":None,
+                "datastream_id":ds_id,
+                "featureofintrest_link":None,}
+
+                print(observation)
+                kafka_avro_produce(avroProducer, topic,observation)
             return success_response_object,success_code
 
         except Exception as e:
@@ -98,4 +167,5 @@ def create_app(script_info=None):
             return failure_response_object,failure_code
 
     return app
+
 
